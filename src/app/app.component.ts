34339132d77c7af5eeb2e194c742812ce96cf224
koiizuka <kohei.iizuka@voicy.jp>
2020-10-22 15:55:54 +0900

    inject my strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { DateAdapter } from '@angular/material/core';
+import { DateRange, MatDatepickerInputEvent, MatDateRangeSelectionStrategy, MAT_DATE_RANGE_SELECTION_STRATEGY } from '@angular/material/datepicker';
+
+@Injectable()
+export class DefaultMatCalendarRangeStrategy<D> implements MatDateRangeSelectionStrategy<D> {
+  constructor(private _dateAdapter: DateAdapter<D>) {}
+
+  selectionFinished(date: D, currentRange: DateRange<D>) {
+    let {start, end} = currentRange;
+
+    if (start == null) {
+      start = date;
+    } else if (end == null && date && this._dateAdapter.compareDate(date, start) >= 0) {
+      end = date;
+    } else {
+      start = date;
+      end = null;
+    }
+
+    if (end != null && start != null && this._dateAdapter.compareDate(date, start) >= 0) {
+      console.log('this');
+    }
+
+    return new DateRange<D>(start, end);
+  }
+
+  createPreview(activeDate: D | null, currentRange: DateRange<D>) {
+    let start: D | null = null;
+    let end: D | null = null;
+
+    if (currentRange.start && !currentRange.end && activeDate) {
+      start = currentRange.start;
+      end = activeDate;
+    }
+
+    return new DateRange<D>(start, end);
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [{
+    provide: MAT_DATE_RANGE_SELECTION_STRATEGY,
+    useClass: DefaultMatCalendarRangeStrategy
+  }]
 })
 export class AppComponent implements OnInit {
 
@@ -23,14 +64,13 @@ export class AppComponent implements OnInit {
       start: new FormControl(),
       end: new FormControl()
     });
+
   }
 
   startlog(event: MatDatepickerInputEvent<Date>) {
-    console.log('start', event.value);
     this.form.get('start').setValue(event.value, { emitEvent: false });
   }
   endlog(event: MatDatepickerInputEvent<Date>) {
-    console.log('end', event.value);
     this.form.get('end').setValue(event.value, { emitEvent: false });
   }
 }
